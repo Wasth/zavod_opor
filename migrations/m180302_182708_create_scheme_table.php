219c154445cfa6600498b4d924ac09c1a8009213
Wasth <riasta@yandex.ru>
2018-03-02 21:38:38 +0300

    New migrations

---
+++ migrations/m180302_182708_create_scheme_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `scheme`.
+ */
+class m180302_182708_create_scheme_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('scheme', [
+            'id' => $this->primaryKey(),
+            'variety_id' => $this->integer(),
+            'content' => $this->text(),
+        ]);
+        $this->createIndex('idx-scheme-variety_id','scheme','variety_id');
+        $this->addForeignKey('fk-scheme-variety_id',
+            'scheme','variety_id',
+            'variety','id');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk-scheme-variety_id','scheme');
+        $this->dropIndex('idx-scheme-variety_id','scheme');
+        $this->dropTable('scheme');
+    }
+}
---
+++ migrations/m180302_182717_create_characs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `characs`.
+ */
+class m180302_182717_create_characs_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('characs', [
+            'id' => $this->primaryKey(),
+            'variety_id' => $this->integer(),
+            'img' => $this->text(),
+        ]);
+        $this->createIndex('idx-characs-variety_id','characs','variety_id');
+        $this->addForeignKey('fk-characs-variety_id',
+            'characs','variety_id',
+            'variety','id');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk-characs-variety_id','characs');
+        $this->dropIndex('idx-characs-variety_id','characs');
+        $this->dropTable('characs');
+    }
+}
