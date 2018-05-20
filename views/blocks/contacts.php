b6c385b4b9a3f7fb43599f764fd37bdf8cee59a1
Rick <riasta@yandex.ru>
2018-05-20 21:45:41 +0300

    Address updated on contacts block and map

--- views/blocks/contacts.php
+++ views/blocks/contacts.php
@@ -2,12 +2,13 @@
     <div id="contactsWrapper">
         <h1 class="block-title">Контакты</h1>
         <div id="contacts" class="content">
-            <div><script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aff0d51dab81683fe5c4f42d2daf56005e597ad2dd3a31f72e14bd0dd20d4bdda&amp;width=100%25&amp;height=452&amp;lang=ru_RU&amp;scroll=true"></script></div>
+            <div><script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A521de30da5c6b84d01e4d1944770929594d1d49ab482f34193ac3206418e8bd9&amp;width=100%25&amp;height=452&amp;lang=ru_RU&amp;scroll=true"></script></div>
             <div>
                 <h2>ООО «Риана Групп»</h2>
                 <h3>Расчет заказа за 30 минут</h3>
                 <h3>Быстрая доставка по всей России</h3>
                 <h3>8 (800) 350-09-19 <br> sales@opora-rg.ru</h3>
+                <h3>Республика Татарстан, г. Лениногорск ул. Промышленная д.1</h3>
             </div>
         </div>
     </div>
