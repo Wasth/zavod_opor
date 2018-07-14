cdbcb613b662996afb6e2de93c70a80c662b6487
Rick <riasta@yandex.ru>
2018-07-14 23:49:06 +0300

    jivosite setted instead of venyoo

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -42,7 +42,11 @@ PublicAsset::register($this);
     </div>
 </div>
 
-<script type="text/javascript" src='//api.venyoo.ru/wnew.js?wc=venyoo/default/science&widget_id=5742335818334208'></script>
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+    (function(){ var widget_id = '2aSx39J4O5';var d=document;var w=window;function l(){
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
 
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
