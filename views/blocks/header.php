13f80f56115217d8719db23e916c9c8509dc1a0b
Rick <riasta@yandex.ru>
2018-10-03 23:27:05 +0300

    Updated menu position and new menu was added

--- views/blocks/header.php
+++ views/blocks/header.php
@@ -10,6 +10,19 @@ use yii\helpers\Url;
                 <h1>«Риана Групп»</h1>
             </a>
         </div>
+        <div id="menuUp">
+            <a href="/">
+                <div class="menu-item">О нас</div>
+            </a><a href="<?= Url::toRoute(['catalog']) ?>">
+                <div class="menu-item">Каталог</div>
+            </a><a href="<?= Url::toRoute(['media']) ?>">
+                <div class="menu-item">Фото и видео</div>
+            </a><a href="<?= Url::toRoute(['contacts']) ?>">
+                <div class="menu-item">Контакты</div>
+            </a><a href="<?= Url::toRoute(['order']) ?>">
+                <div class="menu-item">Заказать</div>
+            </a>
+        </div>
         <div id="right-block">
             <h1 class="number"><span class="number-pre-text">Отправьте заявку на почту <i class="mobile-delimiter fas fa-arrow-right"></i></span><a href="mailto:sales@opora-rg.ru" onclick="yaCounter48355565.reachGoal('emailclick'); return true;"> sales@opora-rg.ru </a></h1>
             <h1 class="email"><span class="mail-pre-text">Есть вопросы? Звоните прямо сейчас!</span>
@@ -18,18 +31,18 @@ use yii\helpers\Url;
         <div class="clear"></div>
     </div>
     <div id="menu">
-        <div id="menuWrapper" class="">
-            <div id="menuContainer" class="content">
+        <div id="menuWrapper">
+            <div class="content">
                 <a href="/">
-                    <div class="menu-item">Главная</div>
-                </a><a href="<?= Url::toRoute(['catalog']) ?>">
-                    <div class="menu-item">Каталог</div>
-                </a><a href="<?= Url::toRoute(['media']) ?>">
-                    <div class="menu-item">Фото и видео</div>
-                </a><a href="<?= Url::toRoute(['contacts']) ?>">
-                    <div class="menu-item">Контакты</div>
-                </a><a href="<?= Url::toRoute(['order']) ?>">
-                    <div class="menu-item">Заказать</div>
+                    <div class="menu-item">Опоры</div>
+                </a><a href="#">
+                    <div class="menu-item">Закладные детали</div>
+                </a><a href="#">
+                    <div class="menu-item">Лестницы и ограждения</div>
+                </a><a href="#">
+                    <div class="menu-item">Сальники</div>
+                </a><a href="#">
+                    <div class="menu-item">Фундаментальные болты</div>
                 </a>
             </div>
         </div>
