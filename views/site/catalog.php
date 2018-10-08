d1a81e2a1068db2e4d1fa40d260faf61447e0507
Wasth <riasta@yandex.ru>
2018-10-08 15:34:46 +0300

    Some edits in contacts block

--- views/site/catalog.php
+++ views/site/catalog.php
@@ -4,18 +4,41 @@
         <h1 class="block-title">Каталог опор</h1>
         <br><br>
         <div id="catalog" class="content">
-            <div id="catalogGrid">
-                <?php use yii\helpers\Url;
-
-                foreach($items as $item): ?>
-                    <div>
-                        <img src="<?= $item->getImgUrl() ?>" alt="catalog_img">
-                        <p><?= $item->text ?></p>
-                        <a href="<?= Url::toRoute(['item/'.$item->id]) ?>">
-                            <button>ПОДРОБНЕЕ</button>
-                        </a>
-                    </div>
-                <?php endforeach; ?>
+<!--            <div id="catalogGrid">-->
+<!--                --><?php //use yii\helpers\Url;
+//
+//                foreach($items as $item): ?>
+<!--                    <div>-->
+<!--                        <img src="--><?//= $item->getImgUrl() ?><!--" alt="catalog_img">-->
+<!--                        <p>--><?//= $item->text ?><!--</p>-->
+<!--                        <a href="--><?//= Url::toRoute(['item/'.$item->id]) ?><!--">-->
+<!--                            <button>ПОДРОБНЕЕ</button>-->
+<!--                        </a>-->
+<!--                    </div>-->
+<!--                --><?php //endforeach; ?>
+<!--                <br>-->
+<!---->
+<!--            </div>-->
+            <div>
+                <h2>Опора 1</h2>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+            </div>
+            <div>
+                <h2>Опора 1</h2>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+            </div>
+            <div>
+                <h2>Опора 1</h2>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
+                <h3 class="inline"><a href="">awd</a></h3>
             </div>
         </div>
     </div>
