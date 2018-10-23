fc01933b5ff66637fb2faaa608cd27764523adaa
Wasth <riasta@yandex.ru>
2018-10-23 10:32:30 +0300

    Added chat

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ PublicAsset::register($this);
     <meta name="mailru-domain" content="XR0aAwLFXBZomEn0" />
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
     <?php $this->head() ?>
-
+    <meta name="yandex-verification" content="01ad9dd2f4f692b5" />
 </head>
 <body>
 
