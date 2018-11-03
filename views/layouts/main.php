0f741463cbe9290a266ca3893978bcf873f237e2
riasta@yandex.ru <riasta@yandex.ru>
2018-11-03 12:28:52 +0300

    Added description meta tag

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,6 +28,7 @@ PublicAsset::register($this);
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
     <?php $this->head() ?>
     <meta name="yandex-verification" content="01ad9dd2f4f692b5" />
+    <meta name="description" content="Риана Групп - ЗАВОД ПО ПРОИЗВОДСТВУ ОПОР ТРУБОПРОВОДОВ">
 </head>
 <body>
 
